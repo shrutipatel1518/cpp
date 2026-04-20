f7bcbff7798589a9cd96cb9eb35506f784aded91
shruti patel <shrutipatel5977@gmail.com>
2026-04-20 13:28:20 +0530

    Add files via upload

---
+++ array/basic1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+    
+int main() {
+    int arr[3];
+    cin>> arr[0] >>arr[1] >> arr[2] >>arr[3]; 
+    cout<<arr[0]<<arr[1] <<arr[2] <<arr[3];
+return 0;
+}
